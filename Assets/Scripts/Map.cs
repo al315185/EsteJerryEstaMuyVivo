4207b5499c15d46312e7175f7b52a2ad01a28c39
dharach <al315185@uji.es>
2017-11-24 18:29:27 +0100

    Mapa y Raton
    Creado mapa y script para que el raton reconozca que hay debajo de él

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject HexPrefab;
+
+    int height = 20;
+    int width = 20;
+
+    float xOffset = 1.77f;
+    float zOffset = 1.51f;
+
+
+	// Use this for initialization
+	void Start () {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float xPos = x*xOffset
+                    ;
+
+                if( y % 2 == 1)
+                {
+                    xPos += xOffset/2f;
+                }
+                GameObject hex_go = (GameObject)Instantiate(HexPrefab, new Vector3(xPos, 0, y*zOffset), Quaternion.identity);
+                hex_go.name = "Hex_" + x + "_" + y;
+
+                hex_go.transform.SetParent(this.transform);
+            }
+        }
+        
+
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/mouseManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            GameObject hitObject = hitInfo.collider.transform.gameObject;
+            Debug.Log("Raycast hit" + hitObject.name);
+
+            if(Input.GetMouseButton(0))
+            {
+                MeshRenderer mr = hitObject.GetComponentInChildren<MeshRenderer>();
+
+                mr.material.color = Color.red;
+            }
+        }
+    }
+}
